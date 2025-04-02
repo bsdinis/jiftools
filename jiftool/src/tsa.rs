3c0bf9f4c2484a146b6940f0972c8b92cd4359c7
Baltasar Dinis <bsdinis@mit.edu>
2025-04-02 23:12:38 +0100

    jiftool: handle accesses out of the original jif

--- jiftool/src/tsa.rs
+++ jiftool/src/tsa.rs
@@ -2,25 +2,20 @@ use jif::ord::OrdChunk;
 use jif::Jif;
 use tracer_format::TimestampedAccess;
 
-fn to_ord_chunk(jif: &Jif, access: TimestampedAccess) -> OrdChunk {
-    OrdChunk::new(
-        access.usecs as u64,
-        access.raw_addr() as u64,
-        1,
-        jif.resolve(access.masked_addr() as u64)
-            .unwrap_or_else(|| {
-                panic!(
-                    "Warning: unresolved address in ordering data: {}",
-                    access.masked_addr()
-                )
-            })
-            .source,
-    )
+fn to_ord_chunk(jif: &Jif, access: TimestampedAccess) -> Option<OrdChunk> {
+    jif.resolve(access.masked_addr() as u64).map(|interval| {
+        OrdChunk::new(
+            access.usecs as u64,
+            access.raw_addr() as u64,
+            1,
+            interval.source,
+        )
+    })
 }
 
 /// construct the ord chunks from the timestamped log
 pub(crate) fn construct_ord_chunks(jif: &Jif, log: Vec<TimestampedAccess>) -> Vec<OrdChunk> {
     log.into_iter()
-        .map(|access| to_ord_chunk(jif, access))
+        .filter_map(|access| to_ord_chunk(jif, access))
         .collect()
 }
